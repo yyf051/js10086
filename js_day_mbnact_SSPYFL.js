892021ad2bd5d65e05161dba83a8c4ca07cd9ab5
dev007 <wjs876046992@gmail.com>
2022-09-30 12:47:23 +0800

    刷视频赢福利活动ID修改

--- js_day_mbnact_SSPYFL.js
+++ js_day_mbnact_SSPYFL.js
@@ -12,15 +12,15 @@ const $ = new Env('江苏移动_刷视频赢福利')
   for (let i = 0; i < options.length; i++) {
     await initCookie($, i)
 
-    // https://wap.js.10086.cn/mb_nact/new/yxwap/entitle/preconditions?actNum=700001067&_t=1647156676735
+    // https://wap.js.10086.cn/mb_nact/new/yxwap/entitle/preconditions?actNum=700002915&_t=1647156676735
     console.log(`${$.accountName}检查参与资格......`)
-    const conditionRet = await mbactFunc($, 'entitle/preconditions', '700001067')
+    const conditionRet = await mbactFunc($, 'entitle/preconditions', '700002915')
     if (!conditionRet) {
       continue
     }
 
     console.log(`${$.accountName}检查参与状态......`)
-    const initRet = await mbactFunc($, 'checkEntitleAccount', '700001067')
+    const initRet = await mbactFunc($, 'checkEntitleAccount', '700002915')
     if (!initRet) {
       continue
     }
@@ -30,13 +30,13 @@ const $ = new Env('江苏移动_刷视频赢福利')
       continue
     }
     for (let j = 0; j < initRet.surplusCount; j++) {
-      const checkCanLottery = await mbactFunc($, 'checkCanLottery', '700001067')
+      const checkCanLottery = await mbactFunc($, 'checkCanLottery', '700002915')
       if (!checkCanLottery) {
         continue
       }
       await $.wait(1000)
 
-      const lotteryStrengthen = await mbactFunc($, 'lotteryStrengthen', '700001067&sourcesNum=H5&featureCode=')
+      const lotteryStrengthen = await mbactFunc($, 'lotteryStrengthen', '700002915&sourcesNum=H5&featureCode=')
       if (!lotteryStrengthen) {
         continue
       }
