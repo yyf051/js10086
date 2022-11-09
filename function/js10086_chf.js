37556ffab6de80d0e2a8790fbf2b54aff5238a92
Herman Wu <hermanwu@HermandeMac.local>
2022-11-08 20:18:02 -0800

    查话费_dep

--- function/js10086_chf.js
+++ function/js10086_chf.js
@@ -23,7 +23,7 @@ console.log(`\n====================共${CookieJDs.length}个移动账号Cookie==
 console.log(`==================脚本执行- 北京时间(UTC+8)：${new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000).toLocaleString('zh', {hour12: false}).replace(' 24:',' 00:')}=====================\n`)
 if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {};
 for (let i = 0; i < CookieJDs.length; i++) {
-  if (!CookieJDs[i].match(/phone=(.+?);/) || !CookieJDs[i].match(/psswd=(.+?);/)) console.log(`\n提示:环境变量 【${CookieJDs[i]}】填写不规范,可能会影响部分脚本正常使用。正确格式为: phone=xxx;psswd=xxx;（分号;不可少）\n`);
+  if (!CookieJDs[i].match(/phone=(.+?);/) || !CookieJDs[i].match(/passwd=(.+?);/)) console.log(`\n提示:环境变量 【${CookieJDs[i]}】填写不规范,可能会影响部分脚本正常使用。正确格式为: phone=xxx;passwd=xxx;（分号;不可少）\n`);
   const index = (i + 1 === 1) ? '' : (i + 1);
   exports['CookieJD' + index] = CookieJDs[i].trim();
 }
