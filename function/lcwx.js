de67ab0c3eda02e0779ba37eed8e483e06b80ce1
Herman Wu <hermanwu@HermandeMac.local>
2023-01-14 14:47:27 +0800

    wx notice log

--- function/lcwx.js
+++ function/lcwx.js
@@ -8,6 +8,7 @@ const kam_token = process.env.KAM_TOKEN || ''
 
 function sendNotice(msg, to_wxids) {
 	if (!msg || !kam_addr || !kam_wxid || !to_wxids || to_wxids.length == 0) {
+		console.log(msg, kam_addr, kam_wxid, kam_token, JSON.stringify(to_wxids))
 		return
 	}
 	for (let i = 0, j = to_wxids.length; i < j; i++) {
