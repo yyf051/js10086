01791ac9d616bf9c08a52b7d780b2efa8230761e
Herman Wu <hermanwu@HermandeMac.local>
2023-02-02 16:51:46 +0800

    查找活动

--- 01js10086_nact_CZHD.js
+++ 01js10086_nact_CZHD.js
@@ -1,21 +1,22 @@
+/*
+http://wap.js.10086.cn/nact/resource/xxxx/html/index.html?shareToken=dQEWCORLKHrkeV2QtW/TUg==&rm=ydc
+江苏移动_查找活动
+cron:1 1 1 1 1
+*/
 const Env = require('./function/01Env')
-const { options, initCookie } = require('./function/01js10086_common')
-const { nactFunc } = require('./function/01js10086_nact')
 
-const $ = new Env('江苏移动_抽奖天天乐')
+const $ = new Env('江苏移动_查找活动')
 !(async () => {
   $.msg = ''
   for (let i = 2460; i < 2470; i++) {
     const url = `https://wap.js.10086.cn/nact/resource/${i}/html/index.html`
     $.get({url}, async (err, desp, data) => {
       if (data && data.indexOf('404') == -1) {
-        console.log(i)
+        console.log(`${url}\n`)
       }
     })
     await $.wait(10000)
   }
-  // console.log(`通知内容：\n\n`, $.msg)
-  // await $.sendNotify($.name, $.msg)
 })().catch((e) => {
   $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')
 }).finally(() => {
