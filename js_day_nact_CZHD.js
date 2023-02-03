f76657511c49a670dea5b4c193983d725a9c2007
Herman Wu <hermanwu@HermandeMac.local>
2023-02-03 16:54:33 +0800

    CZHD

--- js_day_nact_CZHD.js
+++ js_day_nact_CZHD.js
@@ -8,14 +8,14 @@ const Env = require('./function/01Env')
 const $ = new Env('江苏移动_查找活动')
 !(async () => {
   $.msg = ''
-  for (let i = 2460; i < 2470; i++) {
+  for (let i = 2571; i <= 2600; i++) {
     const url = `https://wap.js.10086.cn/nact/resource/${i}/html/index.html`
     $.get({url}, async (err, desp, data) => {
       if (data && data.indexOf('404') == -1) {
-        console.log(`${url}\n`)
+        console.log(`${url}`)
       }
     })
-    await $.wait(10000)
+    await $.wait(20000)
   }
 })().catch((e) => {
   $.log('', `❌ ${$.name}, 失败! 原因: ${e}!`, '')
